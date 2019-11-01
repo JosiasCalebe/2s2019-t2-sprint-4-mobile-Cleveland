271196538cc0f77d6e4176387ab46e383af375e3
Diego Alvarez Colmenero <46938238804@info.local>
2019-11-01 10:28:21 -0300

    Backend and SQL added

---
+++ Backend/Senai.Cleveland.WebApi/Senai.Cleveland.WebApi/Interfaces/IEspecialidadeRepository.cs
@@ -0,0 +1,14 @@
+﻿using Senai.Cleveland.WebApi.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.Cleveland.WebApi.Interfaces
+{
+    interface IEspecialidadeRepository
+    {
+        void Cadastrar (Especialidades especialidade);
+        List<Especialidades> Listar();
+    }
+}
---
+++ Backend/Senai.Cleveland.WebApi/Senai.Cleveland.WebApi/Interfaces/IMedicoRepository.cs
@@ -0,0 +1,16 @@
+﻿using Senai.Cleveland.WebApi.Domains;
+using Senai.Cleveland.WebApi.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.Cleveland.WebApi.Interfaces
+{
+    interface IMedicoRepository
+    {
+        List<Medicos> Listar();
+
+        List<MedicoViewModel> ListarMedicoViewModel();
+    }
+}
